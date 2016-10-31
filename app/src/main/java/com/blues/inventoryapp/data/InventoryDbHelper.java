c70dea02f4df4ebd8bc124724dff05a759b1e403
blues <blues@blues.com>
2016-10-31 10:50:12 +0800

    Inventroy dbHelper finished

---
+++ app/src/main/java/com/blues/inventoryapp/data/InventoryDbHelper.java
@@ -0,0 +1,40 @@
+package com.blues.inventoryapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.blues.inventoryapp.data.InventoryContract.InventoryEntry;
+
+/**
+ * Created by blues on 2016/10/31.
+ */
+
+public class InventoryDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = InventoryDbHelper.class.getSimpleName();
+
+    private static final String DATABASE_NAME = "shelter.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public InventoryDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_INVENTORIES_TABLE = "CREATE TABLE " + InventoryEntry.TABLE_NAME + " ("
+                + InventoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + InventoryEntry.COLUMN_INVENTORY_NAME + " TEXT NOT NULL, "
+                + InventoryEntry.COLUMN_INVENTORY_PRICE + " INTEGER NOT NULL, "
+                + InventoryEntry.COLUMN_INVENTORY_QUANTITY + " INTEGER NOT NULL DEFAULT 0);";
+
+        db.execSQL(SQL_CREATE_INVENTORIES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
