8fefa2e955bacc9fb71efa869ce706d0247fa7ac
blues <blues@blues.com>
2016-11-01 13:56:51 +0800

    finished layout and adapter

---
+++ app/src/main/java/com/blues/inventoryapp/InventoryAdapter.java
@@ -0,0 +1,45 @@
+package com.blues.inventoryapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.blues.inventoryapp.data.InventoryContract.InventoryEntry;
+
+/**
+ * Created by blues on 2016/11/1.
+ */
+
+public class InventoryAdapter extends CursorAdapter {
+    public InventoryAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, viewGroup, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView nameTv = (TextView) view.findViewById(R.id.name);
+        TextView priceTv = (TextView) view.findViewById(R.id.price);
+        TextView quantityTv = (TextView) view.findViewById(R.id.quantity);
+
+        int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_NAME);
+        int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVENTORY_QUANTITY);
+
+        String name = cursor.getString(nameColumnIndex);
+        int price = cursor.getInt(priceColumnIndex);
+        int quantity = cursor.getInt(quantityColumnIndex);
+
+        nameTv.setText(name);
+        priceTv.setText(price);
+        quantityTv.setText(quantity);
+    }
+}
--- app/src/main/java/com/blues/inventoryapp/MainActivity.java
+++ app/src/main/java/com/blues/inventoryapp/MainActivity.java
@@ -1,13 +1,58 @@
 package com.blues.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.blues.inventoryapp.data.InventoryContract.InventoryEntry;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setListView();
+    }
+
+    private void setListView(){
+        ListView inventoryListView = (ListView) findViewById(R.id.list);
+
+        View emptyView = findViewById(R.id.empty_view);
+        inventoryListView.setEmptyView(emptyView);
+        //// TODO: 2016/11/1 adapter 
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {
+                InventoryEntry._ID,
+                InventoryEntry.COLUMN_INVENTORY_NAME,
+                InventoryEntry.COLUMN_INVENTORY_PRICE,
+                InventoryEntry.COLUMN_INVENTORY_QUANTITY
+        };
+
+        return new CursorLoader(this,
+                InventoryEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
