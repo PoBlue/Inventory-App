5eabc3699f2f7d609c556bafce60887a99c0278d
blues <blues@blues.com>
2016-11-01 09:01:03 +0800

    Inventroy provider fixed

--- app/src/main/java/com/blues/inventoryapp/data/InventoryProvider.java
+++ app/src/main/java/com/blues/inventoryapp/data/InventoryProvider.java
@@ -70,7 +70,15 @@ public class InventoryProvider extends ContentProvider {
     @Nullable
     @Override
     public String getType(Uri uri) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match){
+            case INVENTORIES:
+                return InventoryEntry.CONTENT_LIST_TYPE;
+            case INVENTORY_ID:
+                return InventoryEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri + " with match " + match);
+        }
     }
 
     @Nullable
